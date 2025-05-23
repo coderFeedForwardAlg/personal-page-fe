e91602d7ece2542c4df1e5051bbb247487906ce7
coderFeedForwardAlg <gaspersscottma@gmail.com>
2025-05-23 14:41:30 -0700

    typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 export const metadata: Metadata = {
     title: 'Max Gaspers Scott',
-    description: 'All you neet to know about me',
+    description: 'All you need to know about me',
 };
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
